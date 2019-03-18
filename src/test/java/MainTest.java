b0a821251dab85d38ad85a0cfa2c5fe3bafb1a46
samgoldman <sgoldman216@gmail.com>
2019-03-17 22:26:39 -0400

    Messing with headless

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -33,6 +33,13 @@ public class MainTest extends ApplicationTest {
 
     @Before
     public void setUp () throws Exception {
+        if (Boolean.getBoolean("headless")) {
+            System.setProperty("testfx.robot", "glass");
+            System.setProperty("testfx.headless", "true");
+            System.setProperty("prism.order", "sw");
+            System.setProperty("prism.text", "t2k");
+            System.setProperty("java.awt.headless", "true");
+        }
     }
 
     @After
