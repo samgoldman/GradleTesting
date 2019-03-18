2d821b5da53b6e7662bd96252535acd798a70498
samgoldman <sgoldman216@gmail.com>
2019-03-17 22:50:40 -0400

    Travis with head

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -33,13 +33,6 @@ public class MainTest extends ApplicationTest {
 
     @Before
     public void setUp () throws Exception {
-        if (Boolean.getBoolean("headless")) {
-            System.setProperty("testfx.robot", "glass");
-            System.setProperty("testfx.headless", "true");
-            System.setProperty("prism.order", "sw");
-            System.setProperty("prism.text", "t2k");
-            System.setProperty("java.awt.headless", "true");
-        }
     }
 
     @After
