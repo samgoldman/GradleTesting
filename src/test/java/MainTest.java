b69064f1841dd306d4a3d02d8d5211443262d4a5
samgoldman <sgoldman216@gmail.com>
2019-03-20 23:12:52 -0400

    Uploading to Ravana

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,10 +1,12 @@
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Point2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,6 +18,10 @@ import org.loadui.testfx.GuiTest;
 import testclassifications.UiTest;
 import testclassifications.FastTest;
 import testclassifications.SlowTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 import static org.testfx.api.FxAssert.verifyThat;
 import static org.hamcrest.Matchers.is;
@@ -70,4 +76,28 @@ public class MainTest extends ApplicationTest {
         clickOn("#applyButton");
         verifyThat(label.getText(), is("This is a test!"), informedErrorMessage(this));
     }
+
+//    @Category(SlowTest.class)
+//    @Test
+//    public void randomTest () {
+//        List<Window> windows = this.listWindows();
+//
+//        verifyThat(windows.size(), is(1), informedErrorMessage(this));
+//
+//        Window window = windows.get(0);
+//
+//        verifyThat(window.widthProperty().doubleValue(), is(600.0), informedErrorMessage(this));
+//        verifyThat(window.heightProperty().doubleValue(), is(400.0), informedErrorMessage(this));
+//
+//        double x = window.getX();
+//        double y = window.getY();
+//
+//        Random random = new Random();
+//
+//        long start = System.nanoTime();
+//        while (System.nanoTime() - start < 5_000_000_000L) {
+//            Point2D point = new Point2D((float) random.nextInt(600) + x, (float) random.nextInt(400) + y);
+//            this.clickOn(point);
+//        }
+//    }
 }
